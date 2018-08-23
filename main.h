e939476e82c014717d687a589ba48fb1a069bf52
alien <2551523249@qq.com>
2018-08-23 17:10:06 +0800

    计较main.h

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+#define uint unsigned int
+#define uchar unsigned char
+
+#include"reg52.h"
+#include"write_ds1302_byte.h"//写一个字节
+#include"write_ds1302.h"	   //写多个字节
+#include"read_ds_1302.h" 		//读取
+#include"read_rtc.h"			//读取时间
+#include"set_rtc.h"			   //调整时间
+#include"display.h"			  //显示时间
+#include"delay.h"d			 //ms延时函数
+
+
+
+#endif
